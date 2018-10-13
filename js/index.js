5e241fba08cdff8fb4960e47a675d0de5129d0d4
braj011 <bhagyaraj01@gmail.com>
2018-10-13 15:09:57 +0100

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+
+//  done in the console
+
+https://learn.co/tracks/web-development-immersive-2-0-module-three/javascript-foundations/the-dom/listening-to-nodes
